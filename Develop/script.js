1b0beb67f078e9d132adc1381998e57508c1a192
sboucher <sboucher2011@gmail.com>
2021-10-31 09:58:35 -0400

    java structure created

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,57 @@
-// Assignment code here
+//-----------------------------------------------
+// Variable Setup
+//-----------------------------------------------
+var length = 8;
+var upper = true;
+var lower = true;
+var number = true;
+var symbol = true;
+var passwordArray = [];
 
+//-----------------------------------------------
+// Generate Random Variable for Selected Criteria
+//-----------------------------------------------
+function generateVariable(selector) {
+  switch(selector) {
+    //uppercase generation
+    case 'upperCase':
+      passwordArray.push("A");
+      break;
+    //lowercase generation
+    case 'lowerCase':
+      passwordArray.push("b");
+      break;
+    //number generation
+    case 'numbers':
+      passwordArray.push('3');
+      break;
 
+    //symbol generation
+    default: 
+      passwordArray.push('!')
+      break;
+  }
+}
+
+//-----------------------------------------------
+// Randomize Array
+//-----------------------------------------------
+
+//-----------------------------------------------
+// Generate Passowrd Based on User Input
+//-----------------------------------------------
+function generatePassword() {
+  
+  generateVariable('upperCase');
+  generateVariable('lowerCase');
+  generateVariable('numbers');
+  generateVariable('symbols')
+
+  //print to the screen withouth any charecters in array
+  return passwordArray.join("");
+}
+
+//KEEP ALL OF THIS
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +66,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//END OF KEEP ALL OF THIS
+
+
+
+// POPUP - 
+/*
+function openWindow() {
+  var i, l, options = [{
+     value: 'first',
+     text: 'First'
+  }, {
+     value: 'second',
+     text: 'Second'
+  }],
+  newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");  
+
+  newWindow.document.write("<select onchange='window.opener.setValue(this.value);'>");
+  for(i=0,l=options.length; i<l; i++) {
+      newWindow.document.write("<option value='"+options[i].value+"'>");  
+      newWindow.document.write(options[i].text);  
+      newWindow.document.write("</option>");
+  }
+  newWindow.document.write("</select>");
+}
+*/
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Password Generator Starter Code
+# Challenge03
+
+## Purpose
+This website is to generate unique passwords that contain any or all of the follow: upper-case letters, lower-case letter, symbols, and numbers.  The user is also able to enter the desired password length as long as it is between 8 to 128 charecters. 
+
+This website was challenge 03 of Winter 2021 UCF's Full Stack boot camp.  The purpose of this assigment was to create a password generator that accepts user defined critera.
+
+## Website Preview
+![alt text](https://github.com/sboucher2011/challenge03/blob/main/assets/images/website_image_for_readme.png)
+
+## Built With
+* HTML
+* CSS
+* Javascript
+
+## Website
+https://sboucher2011.github.io/challenge03/
+
+## Contribution
+Updated with ❤️ by Shawna Boucher
